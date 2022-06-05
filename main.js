6787f7b7dc018543a880fe1cdfda562cb8bae617
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-05 21:53:10 +0200

    add basic event listener in main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('button');
+const section = document.querySelector('section');
+
+
+const codesGenerator = () => {
+
+
+
+}
+
+document.addEventListener("click", codesGenerator)
